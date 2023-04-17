16db8db3b22824eb476cdf71b026a28fd26e7ec5
baitutanglj <2919588049@qq.com>
2023-04-17 16:00:34 +0800

    commit setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="molprop",
+    version="1.0",
+    description="This is molprop. A package to predict the molecular property.",
+    author="linjie",
+    packages=find_packages(),
+    include_package_data=True,
+)
